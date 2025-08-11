1faca9a59bdcaadca67a481879b092b931699140
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2025-08-11 09:17:41 +0300

    update

--- src/components/UI/Forms/FormOrder.jsx
+++ src/components/UI/Forms/FormOrder.jsx
@@ -12,9 +12,9 @@ export function FormOrder() {
                 </div>
                 <input type="text" placeholder="Адрес проживания" className="border border-gray rounded-4xl p-4 focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
                 <div className="flex flex-row gap-4">
-                    <input type="text" placeholder="Номер телефона" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
-                    <input type="text" placeholder="mm / yy" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
-                    <input type="text" placeholder="cvv" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
+                    <input type="text" placeholder="Номер карточки" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
+                    <input type="text" placeholder="MM / YY" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
+                    <input type="text" placeholder="CVV" className="border border-gray rounded-4xl p-4 w-full focus:outline-none focus:border-tiny focus:ring-1 focus:ring-tiny"/>
                 </div>
             </form>
         </>
